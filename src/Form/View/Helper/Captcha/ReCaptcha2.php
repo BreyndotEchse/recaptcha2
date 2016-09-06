0ff22936de1063663fdeb17730e9416bf27b44bb
Marvin Feldmann <marvin.feldmann@netigo.de>
2016-09-06 17:44:28 +0200

    Fix CS warning

--- src/Form/View/Helper/Captcha/ReCaptcha2.php
+++ src/Form/View/Helper/Captcha/ReCaptcha2.php
@@ -32,7 +32,8 @@ class ReCaptcha2 extends FormInput
 
         if ($captcha === null || !$captcha instanceof AdapterInterface) {
             throw new Exception\DomainException(sprintf(
-                '%s requires that the element has a "captcha" attribute implementing Zend\Captcha\AdapterInterface; none found',
+                '%s requires that the element has a "captcha" attribute implementing %s; none found',
+                AdapterInterface::class,
                 __METHOD__
             ));
         }

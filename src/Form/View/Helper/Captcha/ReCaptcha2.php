98b24fe93df80069f1c89e0af3c35ee501283018
Marvin Feldmann <marvin.feldmann@netigo.de>
2016-09-07 12:24:57 +0200

    Fix noscript iframe not using siteKey

--- src/Form/View/Helper/Captcha/ReCaptcha2.php
+++ src/Form/View/Helper/Captcha/ReCaptcha2.php
@@ -76,7 +76,7 @@ HTML;
   <div style="width: 302px; height: 422px;">
     <div style="width: 302px; height: 422px; position: relative;">
       <div style="width: 302px; height: 422px; position: absolute;">
-        <iframe src="{$apiPath}/fallback?{$queryString}k={$this->siteKey}"
+        <iframe src="{$apiPath}/fallback?{$queryString}k={$siteKey}"
                 frameborder="0" scrolling="no"
                 style="width: 302px; height:422px; border-style: none;">
         </iframe>
